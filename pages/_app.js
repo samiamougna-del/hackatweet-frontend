80a5ac9cca2752aace559e0259826888afdee1a1
Samia <samiamougna@gmail.com>
2025-11-20 11:03:39 +0100

    c'est quoi ca

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,22 @@
 import '../styles/globals.css';
 import Head from 'next/head';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import tweet from '../reducers/tweet';
+import user from '../reducers/user';
+
+const store = configureStore({
+reducer: { tweet, user },
+});
 
 function App({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <title>Next.js App</title>
-      </Head>
-      <Component {...pageProps} />
-    </>
+return (
+  <Provider store={store}>
+    <Head>
+    <title>Hackatweet</title>
+    </Head>
+    <Component {...pageProps} />
+  </Provider>
   );
 }
 
